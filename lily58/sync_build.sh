8b97041e4f62bc179a335dbd349667e2b65453b1
Brian <brianyu0717@msn.com>
2023-02-16 18:43:47 +0000

    update volume control keys

--- lily58/sync_build.sh
+++ lily58/sync_build.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-rsync -vhr yboen /home/brian/qmk_firmware/keyboards/lily58/keymaps/ && qmk compile -kb lily58 -km yboen
-echo "flash? [y, n]: "
+rsync -vhr yboen /home/brian/qmk_firmware/keyboards/lily58/keymaps/
+echo "flash? [y,n]: "
 read flash
 if [ "$flash" = "y" ] 
 then
-qmk flash -kb lily58 -km yboen
+COMMAND=flash
 else
-echo 'no flash'
+COMMAND=compile
 fi
+qmk $COMMAND -kb lily58 -km yboen
