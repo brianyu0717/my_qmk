cf265be86a49f73c16576e8c6678709a35f8b914
Brian <brianyu0717@msn.com>
2023-03-23 13:37:35 +0000

    remove comment

--- sweep/yboen/keymap.c
+++ sweep/yboen/keymap.c
@@ -27,7 +27,6 @@ enum layer_number {
   _NAV,
   _FN
 };
-// left and right are flipped, right hand is reflected left to right
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( 
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,
