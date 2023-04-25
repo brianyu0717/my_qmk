2a0f1e10f8b92c7c02339c0c1b87747cd0720955
Brian <brianyu0717@msn.com>
2023-04-25 15:56:13 +0100

    swap home/end with caps

--- sweep/yboen/keymap.c
+++ sweep/yboen/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_NAV] = LAYOUT( 
     KC_1, 	 KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0, 
-    KC_CAPS, KC_HOME, KC_END,  KC_PGUP, XXXXXXX,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
+    KC_HOME, KC_END,  KC_CAPS, KC_PGUP, XXXXXXX,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
     KC_LGUI, KC_LALT, KC_LCTL, KC_PGDN, KC_LSFT,         XXXXXXX, KC_ESC,  KC_TAB,  KC_INS,  KC_DEL,
                                _______, KC_SPC,          _______, _______
   ),
