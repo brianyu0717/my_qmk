a3bdf42d7ea203534d322107d9d2e2a365eb32a8
brian <brianyu0717@msn.com>
2025-06-03 17:08:34 +0100

    Update

--- sweep/yboen/keymap.c
+++ sweep/yboen/keymap.c
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_NAV] = LAYOUT( 
     KC_1, 	 KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0, 
-    KC_HOME, KC_END,  KC_CAPS, KC_PGUP, CW_TOGG,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
-    KC_LGUI, KC_LALT, KC_LCTL, KC_PGDN, KC_LSFT,         KC_PSCR, KC_ESC,  KC_TAB,  KC_INS,  KC_DEL,
+    KC_HOME, KC_END,  KC_CAPS, KC_PGUP, _______,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
+    _______, _______, _______, KC_PGDN, CW_TOGG,         KC_PSCR, KC_ESC,  KC_TAB,  KC_INS,  KC_DEL,
                                KC_ENT,  KC_SPC,          _______, _______
   ),
   [_FN] = LAYOUT( 
