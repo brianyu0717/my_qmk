b045437eeef89fb03b8587f7044ef1472ee62070
brian <brianyu0717@msn.com>
2025-06-04 11:24:23 +0100

    use lowered row mods

--- sweep/yboen/keymap.c
+++ sweep/yboen/keymap.c
@@ -9,7 +9,14 @@
 #define MO_SYM MO(_SYM)
 #define MO_NAV MO(_NAV)
 #define K_SPC LT(_FN, KC_SPC)
-#define K_ENT LT(_MODS, KC_ENT)
+#define K_Z MT(MOD_LGUI, KC_Z)
+#define K_X MT(MOD_LALT, KC_X)
+#define K_C MT(MOD_LCTL, KC_C)
+#define K_V MT(MOD_LSFT, KC_V)
+#define K_M MT(MOD_LSFT, KC_M)
+#define K_COMM MT(MOD_LCTL, KC_COMM)
+#define K_DOT MT(MOD_LALT, KC_DOT)
+#define K_SLSH MT(MOD_LGUI, KC_SLSH)
 #define IJ_NXT C(A(KC_RBRC))
 #define IJ_PRV C(A(KC_LBRC))
 
@@ -17,16 +24,15 @@ enum layer_number {
   _QWERTY = 0,
   _SYM,
   _NAV,
-  _FN,
-  _MODS
+  _FN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( 
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH,
-                               K_ENT,   K_SPC,           MO_SYM,  MO_NAV
+    K_Z,     K_X,     K_C,     K_V,     KC_B,            KC_N,    K_M,   K_COMM,  K_DOT,   K_SLSH,
+                               KC_ENT,   K_SPC,           MO_SYM,  MO_NAV
   ),
   [_SYM] = LAYOUT(
     KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, _______,         _______, KC_TILD, KC_CIRC, _______, KC_DQUO,
@@ -36,21 +42,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_NAV] = LAYOUT( 
     KC_1, 	 KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0, 
-    KC_HOME, KC_END,  KC_CAPS, KC_PGUP, _______,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
-    _______, _______, _______, KC_PGDN, CW_TOGG,         KC_PSCR, KC_ESC,  KC_TAB,  KC_INS,  KC_DEL,
-                               KC_ENT,  KC_SPC,          _______, _______
+    KC_HOME, KC_END,  KC_CAPS, KC_PGUP, CW_TOGG,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
+    KC_LGUI, KC_LALT, KC_LCTL, KC_PGDN, KC_LSFT,         KC_PSCR, KC_ESC,  KC_TAB,  KC_INS,  KC_DEL,
+                               _______,  KC_SPC,          _______, _______
   ),
   [_FN] = LAYOUT( 
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
     IJ_NXT,  IJ_PRV,  CGL,     CGR,     KC_VOLU,         _______, _______, _______, KC_F11,  KC_F12,
     UND,     CUT,     CPY,     PST,     KC_VOLD,         _______, _______, _______, _______, _______,
                                KC_MUTE, _______,         _______, _______
-  ),
-  [_MODS] = LAYOUT( 
-    _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,         _______, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
-    _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
-                               _______, _______,         _______, _______
   )
   // [_NEW] = LAYOUT( 
   //   _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,
